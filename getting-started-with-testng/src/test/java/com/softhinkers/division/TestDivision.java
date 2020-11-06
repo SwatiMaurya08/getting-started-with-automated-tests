edef88eb6e3ad253e839ee39271a44a9c53d6c52
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-11-07 00:00:27 +0530

    added testcases in TestDivision class

--- getting-started-with-testng/src/test/java/com/softhinkers/division/TestDivision.java
+++ getting-started-with-testng/src/test/java/com/softhinkers/division/TestDivision.java
@@ -1,5 +1,9 @@
 package com.softhinkers.division;
 
+import com.softhinkers.calculator.division.Division;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  * @author Swati Maurya
  * @company Softhinkers
@@ -7,4 +11,20 @@ package com.softhinkers.division;
  * @date 11/6/2020
  */
 public class TestDivision {
+    Division div = new Division();
+
+    @Test
+    public void TestDivisionInt() {
+        int expectedValue = 10;
+        int actualValue = div.division(100, 10);
+        Assert.assertEquals(actualValue, expectedValue);
+    }
+
+    @Test
+
+    public void TestDivisionFloat() {
+        float expectedValue = 10.0f;
+        float actualValue = div.division(100f, 10.0f);
+        Assert.assertEquals(actualValue, expectedValue);
+    }
 }
