357b9906a50ca3a3ca7ee6ba25b72ac88dab7d29
SwatiMaurya08 <swatimaurya704@gmail.com>
2020-11-06 20:40:12 +0530

    added souce code folder

---
+++ getting-started-with-testng/src/main/java/com/softhinkers/calculator/arithemetic/IArithemetic.java
@@ -0,0 +1,8 @@
+package com.softhinkers.calculator.arithemetic;
+
+public interface IArithemetic extends IArithemeticD{
+
+    public  int addition(int a, int b);
+
+    public float addition(float a, float b);
+}
---
+++ getting-started-with-testng/src/main/java/com/softhinkers/calculator/multiplication/Multiplication.java
@@ -0,0 +1,15 @@
+package com.softhinkers.calculator.multiplication;
+
+public class Multiplication {
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public float multiplication(float a, float b) {
+        return a * b ;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+}
